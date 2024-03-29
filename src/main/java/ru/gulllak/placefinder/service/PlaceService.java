480edd3e39ac46e35ffdd9473bcf33b843b6d116
evgenii <gulllak@yandex.ru>
2024-03-29 20:14:50 +0300

    fix: PlaceFinder v:1.0 PlaceService

--- src/main/java/ru/gulllak/placefinder/service/PlaceService.java
+++ src/main/java/ru/gulllak/placefinder/service/PlaceService.java
@@ -2,8 +2,6 @@ package ru.gulllak.placefinder.service;
 
 import com.google.maps.GeoApiContext;
 import com.google.maps.model.PlaceDetails;
-import com.google.maps.model.PlacesSearchResponse;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import ru.gulllak.placefinder.model.Filter;
 
@@ -11,14 +9,9 @@ import java.io.Serializable;
 import java.util.List;
 
 public interface PlaceService {
-
-    PlacesSearchResponse getPlacesIdByText(GeoApiContext context, String messageText);
-
     PlaceDetails getPlaceDetailsByPlaceId(GeoApiContext context, String placeId);
 
     String getPlacePhoto(String photoReference);
-
-    List<BotApiMethod<? extends Serializable>> getSearchResults(long chatId, String messageText);
 
     List<PartialBotApiMethod<? extends Serializable>> getSearchResults(long chatId, Filter filter);
 }
